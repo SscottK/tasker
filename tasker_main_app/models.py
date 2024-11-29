39bd519eeaeb8e161e169b069c5022fd7ddeb1c5
ScottKinnear <gr8whtninjassk@gmail.com>
2024-11-29 14:48:56 -0800

    Init Commit, Models, PostgresQL

---
+++ tasker_main_app/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+#The status of a Whole list or a single list item
+STATUS_CHOICES = (
+    ('N', 'Not Started'),
+    ('I', 'In Progress'),
+    ('C', 'Complete'),
+)
+
+#Checklist model
+class Checklist(models.Model):
+    #Name of the Cheklist
+    list_name = models.CharField(max_length=50)
+    #Status of the Checklist
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default=STATUS_CHOICES[0][0]
+    )
+    #The owner of the Checklis
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    #Keeping track or creation and updates date and time (to implement leaderboard feature later)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+#List item model
+class Listitem(models.Model):
+    #Step name
+    step_name = models.CharField(max_length=50)
+    #A descriptopn of how to complete the step
+    description = models.TextField(max_length=1000)
+    #List item status
+    status = models.CharField(
+        max_length=1,
+        choices=STATUS_CHOICES,
+        default=STATUS_CHOICES[0][0]
+    )
+    #checlist the list item belongs to
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
+    #keeping trak of date and time for reminders
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+#roles to be assigned when adding users to a checklist
+ROLE_CHOICES = (
+    ('R', 'Ready Only'),
+    ('E', 'Editor'),
+)
+
+
+#Assigning a role to a user that is not the owner of the list
+class List_user(models.Model):
+    #The user being assigned a role
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #The role being assigned to the user
+    role = models.CharField(
+        max_length=1,
+        choices=ROLE_CHOICES,
+        default=ROLE_CHOICES[0][0]
+    )
+    #The Checklist to which the user is being assigned a role for
+    checklist = models.ForeignKey(Checklist, on_delete=models.CASCADE)
+
+
+#Reminders for list items
+class Reminder(models.Model):
+    #List item the reminder belongs to
+    list_item =  models.ForeignKey(Listitem, on_delete=models.CASCADE)
+    #the day the reminder should be sent
+    reminder_date = models.DateTimeField()
+    #The User who created the reminder
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
