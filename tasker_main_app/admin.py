f0aafeb280346b3ad90dc1ddd5e1131492aee514
ScottKinnear <gr8whtninjassk@gmail.com>
2024-12-01 15:17:01 -0800

    registerd models to admin site

--- tasker_main_app/admin.py
+++ tasker_main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Checklist, Listitem, List_user, Reminder
 
 # Register your models here.
+admin.site.register(Checklist)
+admin.site.register(Listitem)
+admin.site.register(List_user)
+admin.site.register(Reminder)
