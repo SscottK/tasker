53132c2cbae0b5b5c52991000e79dab16f0c30a2
Sammy <samanthabeltran1993@gmail.com>
2024-12-03 12:16:37 -0600

    view a checklist done

--- tasker_main_app/urls.py
+++ tasker_main_app/urls.py
@@ -3,7 +3,11 @@ from . import views
 from .views import ChecklistCreate
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='welcome'),
     path('checklists/create/', views.ChecklistCreate.as_view(), name='checklist-create'),
     path('checklists/', views.checklist_index, name='checklist-index'),
+    path('checklists/<int:checklist_id>/', views.checklist_detail, name='checklist-detail'),
+    path('checklists/<int:pk>/update/', views.ChecklistUpdate.as_view(), name='checklist-update'),
+    path('checklists/<int:pk>/delete/', views.ChecklistDelete.as_view(), name='checklist-delete'),
+
 ]
--- tasker_main_app/views.py
+++ tasker_main_app/views.py
@@ -1,22 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Checklist
 from .forms import ChecklistForm
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request, 'welcome.html')
 
+#checklist create view
 class ChecklistCreate(CreateView):
     model = Checklist
     form_class = ChecklistForm
     template_name = 'main_app/checklist_form.html'
-    success_url = '/checklists/'
+    success_url = '/checklists/checklist_id'
 
-
+#view of all checklists
 def checklist_index(request):
     checklists = Checklist.objects.all()
 
     return render(request, 'checklists/index.html', {'checklists': checklists})
+
+
+#view of one checklist
+def checklist_detail(request, checklist_id):
+    checklist = get_object_or_404(Checklist, id=checklist_id)
+
+    return render(request, 'checklists/detail.html', {
+        'checklist': checklist,
+    })
+
+
+#edit checklist
+class ChecklistUpdate(UpdateView):
+    model = Checklist
+    fields = ['list_name', 'status']
+
+#delete checklist
+class ChecklistDelete(DeleteView):
+    model = Checklist
+    success_url = '/checklists/'
