a7f9e0e05b2cf4383cbb8d9d70cc8e92a57eacae
ScottKinnear <gr8whtninjassk@gmail.com>
2024-12-02 18:08:54 -0800

    Add pdeudo code for create reminder and mailer views

--- tasker_main_app/views.py
+++ tasker_main_app/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+#import reminder form
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'welcome.html')
+
+
+#create cron job to call mailer view
+#mailer
+#try
+#get all reminders that need to be sent out in the next 30 mins
+#for all reminders
+#if reminder has not been sent
+#send reminder
+#mark reminder as sent
+#when done reply with ok it worked
+#except
+#if an error occurs reply with error
+
+
+#Create reminder view
+#define create reaminder args. request, user_id, list_item_id
+#assign reminder form request to form variable
+#check to see if form is_valid()
+    #create new reminder variable but do not save anything to it
+    #new reminder = form.save(commit=false)
+    #Add user_id to new reminder
+    #Add list_item_id to new reminder
+    #save new reminder
+#redirect to list detail
+
+
+
+
+#Edit reminder view
+
+#reminders index view
+
+#Delete reminder view
