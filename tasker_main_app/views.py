a2b0e1acc67eba019959d7e2780e3b8064bf435c
ScottKinnear <gr8whtninjassk@gmail.com>
2024-12-03 16:51:32 -0800

    Added views pseudo code back in

--- tasker_main_app/views.py
+++ tasker_main_app/views.py
@@ -85,3 +85,36 @@ class ListitemUpdate(UpdateView):
     def get_success_url(self):
         checklist_id = self.object.checklist.id
         return reverse_lazy('checklist-detail', kwargs={'checklist_id': checklist_id})
+
+
+#mailer
+#try
+#get all reminders that need to be sent out in the next 30 mins
+#for all reminders
+#if reminder has not been sent
+#send reminder
+#mark reminder as sent
+#when done reply with ok it worked
+#except
+#if an error occurs reply with error
+
+
+#Create reminder view
+#define create reaminder args. request, user_id, list_item_id
+#assign reminder form request to form variable
+#check to see if form is_valid()
+    #create new reminder variable but do not save anything to it
+    #new reminder = form.save(commit=false)
+    #Add user_id to new reminder
+    #Add list_item_id to new reminder
+    #save new reminder
+#redirect to list detail
+
+
+
+
+#Edit reminder view
+
+#reminders index view
+
+#Delete reminder view
