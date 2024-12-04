62ac1992ed74244e06cee0d58a9b3f0c64516787
Sammy <samanthabeltran1993@gmail.com>
2024-12-04 14:07:52 -0600

    list item full functioning

--- tasker_main_app/urls.py
+++ tasker_main_app/urls.py
@@ -4,13 +4,13 @@ from .views import ChecklistCreate
 
 urlpatterns = [
     path('', views.home, name='welcome'),
-    path('<checklists/create/', views.ChecklistCreate.as_view(), name='checklist-create'),
+    path('checklists/create/', views.ChecklistCreate.as_view(), name='checklist-create'),
     path('checklists/', views.checklist_index, name='checklist-index'),
     path('checklists/<int:checklist_id>/', views.checklist_detail, name='checklist-detail'),
     path('checklists/<int:pk>/update/', views.ChecklistUpdate.as_view(), name='checklist-update'),
     path('checklists/<int:pk>/delete/', views.ChecklistDelete.as_view(), name='checklist-delete'),
     path('checklists/<int:checklist_id>/add-task', views.add_task_to_checklist, name='add-task'),
     path('checklists/<int:checklist_id>/edit-task/<int:pk>/', views.ListitemUpdate.as_view(), name='edit-task'),
-    path('checklists/<int:checklist_id>/add-task', views.add_task_to_checklist, name='add-task')
+    path('checklists/<int:checklist_id>/delete-task/<int:pk>/', views.ListitemDelete.as_view(), name='delete-task'),
 ]
 
