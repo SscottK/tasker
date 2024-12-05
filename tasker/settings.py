602a1afe4b980df49fd3011e4539618dcd873572
BeauX <fire-fly_7@hotmail.com>
2024-12-05 15:14:23 -0800

    log in/out

--- tasker/settings.py
+++ tasker/settings.py
@@ -100,6 +100,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Login and Logout Redirection
+LOGIN_REDIRECT_URL = '/'  # Redirect to home page after login
+LOGIN_URL = '/login/'  # URL where users are redirected for login
+LOGOUT_REDIRECT_URL = '/'  # Redirect to home page after logout
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- tasker_main_app/urls.py
+++ tasker_main_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 from .views import ChecklistCreate
 
 urlpatterns = [
   path('', views.home, name='welcome'),
   path('signup/', views.signup, name='signup'),
+  path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+  path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
  ]
