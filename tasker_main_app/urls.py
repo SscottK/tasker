815a9a50ff6b6bbc718a12a13191e90d42d35c21
ScottKinnear <gr8whtninjassk@gmail.com>
2024-12-09 17:47:17 -0800

    add Reminder index and create

--- tasker_main_app/urls.py
+++ tasker_main_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
   path('signup/', views.signup, name='signup'),
   path('profile/', views.user_detail, name='user_detail'),
   path('edit/', views.edit_user, name='edit_user'),
-  path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+  path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
   path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 
   path('checklists/<int:checklist_id>/tasks/', views.get_checklist_tasks, name='get-checklist-tasks'),
@@ -24,4 +24,6 @@ urlpatterns = [
   path('checklists/<int:checklist_id>/add-task', views.add_task_to_checklist, name='add-task'),
   path('checklists/<int:checklist_id>/edit-task/<int:pk>/', views.ListitemUpdate.as_view(), name='edit-task'),
   path('checklists/<int:checklist_id>/delete-task/<int:pk>/', views.ListitemDelete.as_view(), name='delete-task'),
+  path('reminders/', views.reminder_index, name='reminders-index'),
+  path('checklists/<int:checklist_id>/new-reminder/<int:list_item_id>/', views.create_reminder, name='new-reminder'), 
  ]
